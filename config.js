da6cbccecebb9027693365c14960a4fc1c236fe7
Olavitsme1 <109358970+Olavitsme1@users.noreply.github.com>
2022-08-04 21:39:32 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 //=============『 Utama 』================== //
-global.owner = ['6287898307350'] 
+global.owner = ['6281319339702'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Gempy⛧⁩⁩'
-global.numberowner = '6287898307350'
-global.instagram = 'https://www.instagram.com/gmpyyy'
-global.github = 'https://github.com/gmpyyy'
-global.dana = '087898307350'
-global.pulsa = '087898307350'
+global.nameowner = '𝐎𝐥𝐚𝐯⛧⁩⁩'
+global.numberowner = '6281319339702'
+global.instagram = 'https://www.instagram.com/'
+global.github = 'https://github.com/Olavitsme1'
+global.dana = '085712789786'
+global.pulsa = '081319339702'
 global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'PENTABOT'
+global.namebot = 'AyuBot'
 global.gc = 'https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2'
 global.web = 'https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/48f203b814f84b34055c8.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/48f203b814f84b34055c8.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'PENTABOT'
 global.watermark = wm
-global.wm2 = '                     「PENTABOT⁩ 」'
-global.wm3 = 'Gempy⛧'
+global.wm2 = '                     「 AYU_BOT 」'
+global.wm3 = '©\nO\nL\nA\nV'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg'
+global.media = 'https://telegra.ph/file/48f203b814f84b34055c8.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = 'Process...'
 global.eror = 'ERROR'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'Created by'
-global.author = 'Gempy⛧⁩⁩'
+global.author = 'O\nL\nA\nV⁩⁩'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
